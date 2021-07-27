ae6f278d1813802c8e8d8c6f806e134f57c5335d
Oleksii Bozhko <oleksii.bozhko@sitecore.com>
2021-07-27 17:05:05 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+
+            Console.WriteLine("Type number & press Enter");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            do
+            {
+                
+            } while (b);
+
+        }
+    }
+}
